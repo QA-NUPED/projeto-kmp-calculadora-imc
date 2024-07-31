af554281cd4fc98cedf8e87bbd41f4a2481c3434
Lucas Almeida <lucas.spereira@stone.com.br>
2024-07-31 16:23:28 -0300

    Adicionando content description para acessibilidade

--- shared/src/androidUnitTest/kotlin/com/example/bmi/Test.android.kt
+++ shared/src/androidUnitTest/kotlin/com/example/bmi/Test.android.kt
@@ -1,12 +1,27 @@
 package com.example.bmi
-//
-//import org.junit.Assert.assertTrue
-//import org.junit.Test
-//
-//class AndroidGreetingTest {
-//
-//    @Test
-//    fun testExample() {
-//        assertTrue("Check Android is mentioned", Greeting().greet().contains("Android"))
-//    }
-//}
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class AndroidResultTest {
+
+    @Test
+    fun testResultNotice() {
+        val weight = 80
+        val height: Double? = null
+        val expectedResult = "Please enter valid numbers"
+
+        val actualResult = calculateBMI(weight, height)
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    private fun calculateBMI(weight: Int, height: Double?): String {
+        if (weight <= 0 || height == null || height <= 0) {
+            return "Please enter valid numbers"
+        }
+        // lógica para calcular o BMI
+        val bmi = weight / (height * height)
+        return "Your BMI is $bmi"
+    }
+}
