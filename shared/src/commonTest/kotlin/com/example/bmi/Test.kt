f2a74a7ec13851d1b41d9113f350f5a5ecf19446
Lucas Almeida <lucas.spereira@stone.com.br>
2024-07-31 12:04:59 -0300

    Adicionando os testes unitarios

--- shared/src/commonTest/kotlin/com/example/bmi/Test.kt
+++ shared/src/commonTest/kotlin/com/example/bmi/Test.kt
@@ -1,12 +1,55 @@
 package com.example.bmi
 
-//import kotlin.test.Test
-//import kotlin.test.assertTrue
-//
-//class CommonGreetingTest {
-//
-////    @Test
-////    fun testExample() {
-////        assertTrue(Greeting().greet().contains("Hello"), "Check 'Hello' is mentioned")
-////    }
-////}
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class BMICalculatorTest {
+
+    private val bmiCalculator = BMICalculator()
+
+    @Test
+    fun testCalculateBMI() {
+        val weight = 70.0
+        val height = 1.75
+        val expectedBMI = 22.86
+
+        val actualBMI = bmiCalculator.calculateBMI(weight, height)
+        assertEquals(expectedBMI, actualBMI, 0.01)
+    }
+
+    @Test
+    fun testUnderweightCategory() {
+        val bmi = 17.0
+        val expectedCategory = "Underweight"
+
+        val actualCategory = bmiCalculator.getBMICategory(bmi)
+        assertEquals(expectedCategory, actualCategory)
+    }
+
+    @Test
+    fun testNormalWeightCategory() {
+        val bmi = 22.0
+        val expectedCategory = "Normal weight"
+
+        val actualCategory = bmiCalculator.getBMICategory(bmi)
+        assertEquals(expectedCategory, actualCategory)
+    }
+
+    @Test
+    fun testOverweightCategory() {
+        val bmi = 27.0
+        val expectedCategory = "Overweight"
+
+        val actualCategory = bmiCalculator.getBMICategory(bmi)
+        assertEquals(expectedCategory, actualCategory)
+    }
+
+    @Test
+    fun testObesityCategory() {
+        val bmi = 32.0
+        val expectedCategory = "Obesity"
+
+        val actualCategory = bmiCalculator.getBMICategory(bmi)
+        assertEquals(expectedCategory, actualCategory)
+    }
+}
